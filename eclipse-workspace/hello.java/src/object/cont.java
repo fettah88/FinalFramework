de45879985400c14b18f13db75316e2cb9341f2a
fettah88 <king.fm.gm@gmail.com>
2022-05-18 01:11:44 -0400

    hello java

---
+++ eclipse-workspace/hello.java/src/object/cont.java
@@ -0,0 +1,17 @@
+package object;
+
+public class cont {
+	
+	public static void main(String[] args) {
+	
+		for (int i=0; i<10; i++){
+			
+			if (i> 5 && i<9){
+				
+		      continue;
+		}
+System.out.println(i);
+		
+	}
+  }    
+}
